bf66ff8b1ddf7b53b3b8faf5fcdba5101a6eca60
krishpranav <krisna.pranav@gmail.com>
2023-06-25 18:12:34 +0530

    method: to-int

--- src/utils/src/methods/to-int.ts
+++ src/utils/src/methods/to-int.ts
@@ -1,7 +1,9 @@
 export function toInteger(val: number | string | boolean) {
-    if (typeof val == 'number' && val > Number.MAX_SAFE_INTEGER) {
-        return 0;
+    if (typeof val === 'number' && val > Number.MAX_SAFE_INTEGER) {
+      return 0;
+    } else if (typeof val === 'boolean' && val === true) {
+      return 1;
     } else {
-        return parseInt(`${val}`) || 0;
+      return parseInt(`${val}`) || 0;
     }
 }
