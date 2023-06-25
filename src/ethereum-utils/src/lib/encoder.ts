b55b21e11b10bb2385f3830ccd32162968ae6cdb
krishpranav <krisna.pranav@gmail.com>
2023-06-25 18:29:40 +0530

    eth-utils: lib[encoder]

--- src/ethereum-utils/src/lib/encoder.ts
+++ src/ethereum-utils/src/lib/encoder.ts
@@ -1,15 +1,21 @@
-import { AbiCoder } from "ethers/utils/abi-coder";
-import { getAddress } from "ethers/utils/address";
-import { Encode } from "../types/utils";
+import { AbiCoder } from 'ethers/utils/abi-coder';
+import { getAddress } from 'ethers/utils/address';
+import { Encode } from '../types/utils';
 
 export class Encoder implements Encode {
-    private coder: AbiCoder = new AbiCoder();
 
-    public encodeParameters(type: any, values: any[]): string {
-        return this.coder.encode(types, values);    
-    }
+  private coder: AbiCoder = new AbiCoder();
 
-    decodeParameters(types: any, data: any) {
-        return this.coder.decode(types, data);
-    }
+  public encodeParameters(types: any, values: Array<any>): string {
+    return this.coder.encode(types, values);
+  }
+
+  public decodeParameters(types: any, data: any): any {
+    return this.coder.decode(types, data);
+  }
+
+  public normalizeAddress(address: string): string {
+    return address ? getAddress(address) : null;
+  }
+
 }
