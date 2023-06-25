5db84126f69d2506b2f820429e76c3ca1b5143ba
krishpranav <krisna.pranav@gmail.com>
2023-06-25 18:16:02 +0530

    methods: to-tuple

---
+++ src/utils/src/methods/to-tuple.ts
@@ -0,0 +1,22 @@
+export function toTuple(obj) {
+    if (!(obj instanceof Object)) {
+        return [];
+    }
+
+    const output = [];
+    let i = 0;
+
+    Object.keys(obj).forEach((k) => {
+        if(obj[k] instanceof Object) {
+            output[i] = toTuple(obj[k]);
+        } else if (obj[k] instanceof Array) {
+            let j1 = 0
+            const temp1 = [];
+            obj[k].forEach((ak) => {
+                temp1[j1] = toTuple(obj[k]);
+            })
+        };
+    });
+
+    return output;
+}
