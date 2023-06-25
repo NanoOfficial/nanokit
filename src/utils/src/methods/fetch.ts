4134ff43a753a88a4dd016326dd3026aee005b57
krishpranav <krisna.pranav@gmail.com>
2023-06-25 18:07:52 +0530

    utils: methods[fetch]

--- src/utils/src/methods/fetch.ts
+++ src/utils/src/methods/fetch.ts
@@ -1,13 +1,13 @@
 export async function fetchJson(path, options?): Promise<any> {
     if (typeof window !== 'undefined') {
-        return (window as any).fetch(path, options).then((r) => r.json());
+      return (window as any).fetch(path, options).then((r) => r.json());
     } else if (path.lastIndexOf('http', 0) !== 0) {
-        try {
-            return require('fs');
-        } catch (e) {
-            return null;
-        }
+      try { 
+        return require('fs').promises.readFile(path, 'utf8').then((d) => JSON.parse(d));
+      } catch (e) {
+        return null;
+      }
     } else {
-        return require('node-fetch')(path, options).then((r) => r.json());
+      return require('node-fetch')(path, options).then((r) => r.json());
     }
 }
