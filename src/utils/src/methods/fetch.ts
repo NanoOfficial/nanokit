3dfa6db35239a3c71e9f40371fa77dc15f885715
krishpranav <krisna.pranav@gmail.com>
2023-06-25 18:07:24 +0530

    methods: fetch

--- src/utils/src/methods/fetch.ts
+++ src/utils/src/methods/fetch.ts
@@ -1,1 +1,13 @@
-//
+export async function fetchJson(path, options?): Promise<any> {
+    if (typeof window !== 'undefined') {
+        return (window as any).fetch(path, options).then((r) => r.json());
+    } else if (path.lastIndexOf('http', 0) !== 0) {
+        try {
+            return require('fs');
+        } catch (e) {
+            return null;
+        }
+    } else {
+        return require('node-fetch')(path, options).then((r) => r.json());
+    }
+}
